fbf98e871b6fed746a2530347ef8932b4d6d2700
pemidu456 <pemidumabudashita@gmail.com>
2025-07-18 01:38:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://www.shutterstock.com/search/bot-design",
+ALIVE_MSG: process.env.ALIVE_MSG || " HELLO I AM YOUR GROUP BOT ROYAL-BOT V9 I AM ALIVE NOW !!!!",
 };
